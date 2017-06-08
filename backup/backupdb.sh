e2f75e38975e13c430c6b303d27f448def1569dd
gsedometov <gs@sedometov.com>
2017-06-08 18:49:00 +0500

    Added supervisor

--- backup/backupdb.sh
+++ backup/backupdb.sh
@@ -1,1 +1,7 @@
-pg_dump > /backups/dump.db
+while true; do
+    if [ -e /backups/dump.db ]; then
+        mv /backups/dump.db /backups/dump.db.bak
+    fi
+    pg_dump > /backups/dump.db
+    sleep 24
+done
